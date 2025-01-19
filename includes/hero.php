40f376a1c43555bf39081a409a8904365b35a107
James Alderman <james.alderman@parkersoftware.com>
2025-01-19 16:29:45 +0000

    svg update using flex positioning

--- includes/hero.php
+++ includes/hero.php
@@ -2,14 +2,16 @@
 <section class="hero-el">
     <div>
         <h1>One platform for your team and your work</h1>
-     
+    
             <div class="para-el">
-            <svg width="20" height="20" class="c-message__file_icon" fill="#1d1c1d" xmlns="http://www.w3.org/2000/svg"><title></title><path fill-rule="evenodd" clip-rule="evenodd" d="M8 16c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm-1.59-4.18c.29 0 .57-.13.76-.36l4.59-5.43c.15-.17.23-.4.23-.63 0-.3-.12-.59-.36-.78-.19-.15-.4-.22-.65-.22-.29 0-.56.12-.75.35L6.49 9.19l-.83-1.17a.995.995 0 00-.81-.42c-.21 0-.4.06-.57.17-.28.18-.43.46-.43.81 0 .21.06.41.18.59l1.55 2.21c.2.28.49.44.83.44z" fill="#4A154B"></path></svg>    
-             Level up your teamwork with benefits such as unlimited app integrations, audio and video capabilities and more</div>
-
-            <div class="para-el margin-top">
             <svg width="20" height="20" class="c-message__file_icon" fill="#1d1c1d" xmlns="http://www.w3.org/2000/svg"><title></title><path fill-rule="evenodd" clip-rule="evenodd" d="M8 16c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm-1.59-4.18c.29 0 .57-.13.76-.36l4.59-5.43c.15-.17.23-.4.23-.63 0-.3-.12-.59-.36-.78-.19-.15-.4-.22-.65-.22-.29 0-.56.12-.75.35L6.49 9.19l-.83-1.17a.995.995 0 00-.81-.42c-.21 0-.4.06-.57.17-.28.18-.43.46-.43.81 0 .21.06.41.18.59l1.55 2.21c.2.28.49.44.83.44z" fill="#4A154B"></path></svg>
-             Save 50% for the first three months of Slack Pro
+            <p>Level up your teamwork with benefits such as unlimited app integrations, audio and video capabilities and more</p>    
+            </div>
+
+            <div class="para-el">
+            <svg width="20" height="20" class="c-message__file_icon" fill="#1d1c1d" xmlns="http://www.w3.org/2000/svg"><title></title><path fill-rule="evenodd" clip-rule="evenodd" d="M8 16c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm-1.59-4.18c.29 0 .57-.13.76-.36l4.59-5.43c.15-.17.23-.4.23-.63 0-.3-.12-.59-.36-.78-.19-.15-.4-.22-.65-.22-.29 0-.56.12-.75.35L6.49 9.19l-.83-1.17a.995.995 0 00-.81-.42c-.21 0-.4.06-.57.17-.28.18-.43.46-.43.81 0 .21.06.41.18.59l1.55 2.21c.2.28.49.44.83.44z" fill="#4A154B"></path></svg>
+            <p>Save 50% for the first three months of Slack Pro</p>
+             
             </div>
    
         <button class="margin-top">Buy now, save 50%</button>
