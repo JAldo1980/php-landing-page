f8701d7dc5676d15258633b8b68998171b4e8d57
James Alderman <james.alderman@parkersoftware.com>
2025-01-19 14:50:19 +0000

    images and style file updated

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <a href="/my-php-website/index.php" class="logo">
-        <img src="/path-to-your-logo/logo.png" alt="Slack Logo">
+        <img src="images/slack-salesforce-logo-nav-black@2x.png" alt="Slack Logo">
     </a>
     <div class="nav-links">
         <a href="/my-php-website/contact.php" class="btn-sales">Talk to Sales</a>
